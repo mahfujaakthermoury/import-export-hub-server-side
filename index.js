799fbf9a8674e688db8a6b4f1555fb2e82eddcaf
majfujamoury <mahfujaakther186630gmail.com>
2025-12-24 01:31:37 +0600

    code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -6,9 +6,10 @@ const port = 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 
-const uri = "mongodb+srv://import-export-hub:11Mp3D7F6GqEPmDX@clusterhub.3pf2lfb.mongodb.net/?appName=ClusterHub";
+const uri = "mongodb+srv://import_export:P7diRksOr9QCTsdH@clusterhub.3pf2lfb.mongodb.net/?appName=ClusterHub";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,6 +22,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const database = client.db('exportProduct');
+    const exportProducts = database.collection('products')
+
+    //Post or save product to DB
+    app.post('/products', async (req, res) =>{
+      const data = req.body;
+      console.log(data);
+      const result = await exportProducts.insertOne(data);
+      res.send(result ) 
+    })
+
+    // Get products from DB
+    app.get('/products', async(req, res)=>{
+      const result = await exportProducts.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/products/:id', async(req, res)=>{
+      const id = req.params
+      console.log(id);
+
+      const query = {_id: new ObjectId(id)}
+      const result = await exportProducts.findOne(query)
+      res.send(result)
+      
+    })
+
+    app.get('/my-export', async(req, res)=>{
+      const {email} = req.query
+     
+      const query = {email: email}
+      const result = await exportProducts.find(query).toArray()
+      res.send(result)
+      
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -34,6 +72,6 @@ app.get('/',(req, res)=>{
 })
 
 app.listen(port, ()=>{
-    console.log('Server is running on ${port}');
+    console.log(`Server is running on ${port}`);
     
 })
